8a6f8d633568eaf1998293842a574e2daff4630a
MD Anwar Sayeed <anwar.saeed656@gmail.com>
2023-02-24 12:27:13 +0600

    color

--- practice2.php
+++ practice2.php
@@ -1,32 +1,32 @@
 <?php
 
-class Fund
-{
-    public $fund;
-    function __construct($initialFund = 0)
-    {
-        $this->fund = $initialFund;
-    }
+// class Fund
+// {
+//     public $fund;
+//     function __construct($initialFund = 0)
+//     {
+//         $this->fund = $initialFund;
+//     }
 
-    public function addFund($money)
-    {
-        $this->fund += $money;
-    }
-    public function deduct($money)
-    {
-        $this->fund -= $money;
-    }
-    public function getTotal()
-    {
-        echo "Total fund is {$this->fund}\n";
-    }
-}
+//     public function addFund($money)
+//     {
+//         $this->fund += $money;
+//     }
+//     public function deduct($money)
+//     {
+//         $this->fund -= $money;
+//     }
+//     public function getTotal()
+//     {
+//         echo "Total fund is {$this->fund}\n";
+//     }
+// }
 
-$myFund = new Fund(100);
-$myFund->getTotal();
-$myFund->addFund(10);
-$myFund->deduct(6);
-$myFund->getTotal();
+// $myFund = new Fund(100);
+// $myFund->getTotal();
+// $myFund->addFund(10);
+// $myFund->deduct(6);
+// $myFund->getTotal();
 
 
 class Fund
