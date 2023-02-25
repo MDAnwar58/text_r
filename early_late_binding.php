254236256940a07d073061c943c1559c99665e82
MD Anwar Sayeed <anwar.saeed656@gmail.com>
2023-02-25 22:50:42 +0600

    early late bindings

---
+++ early_late_binding.php
@@ -0,0 +1,50 @@
+<?php
+
+// late bindings
+class Planet
+{
+    static function echoName()
+    {
+        echo static::getName();
+    }
+
+    static function getName()
+    {
+        return "Planet";
+    }
+}
+
+class Earth extends Planet
+{
+    static function getName()
+    {
+        return "Earth";
+    }
+}
+
+Earth::echoName();
+
+
+// early bindings
+class Planet
+{
+    static function echoName()
+    {
+        echo self::getName();
+    }
+
+    static function getName()
+    {
+        return "Planet";
+    }
+}
+
+class Earth extends Planet
+{
+    static function getName()
+    {
+        return "Earth";
+    }
+}
+
+Earth::echoName();
